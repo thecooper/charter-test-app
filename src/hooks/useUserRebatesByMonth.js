6b0c3b3b63bd1dfc164a7af519b0e23740cae85f
Justin Cooper <jcoop.del@gmail.com>
2022-11-13 23:34:23 -0500

    Refactor for simplicity and clarity

--- src/hooks/useUserRebatesByMonth.js
+++ src/hooks/useUserRebatesByMonth.js
@@ -13,23 +13,41 @@ export function useUserRebatesByMonth(user) {
         const { transactions } = user;
 
         const transactionsByMonth = transactions
-            .reduce((lookup, transaction) => {
-                const monthValue = moment(transaction.date).month();
-
-                if (!lookup[monthValue]) {
-                    lookup[monthValue] = 0;
-                }
-
-                const rebateAmount = calculateRebate(transaction);
-                lookup[monthValue] += rebateAmount;
-
-                return lookup;
-            }, [])
-            .map((rebateAmount, monthValue) => ({
-                month: getMonthName(monthValue),
-                rebateAmount
-            }));
+            .reduce(groupTransactionsByMonth, [])
+            .map(mapTotalRebatesPerMonth);
 
         return transactionsByMonth;
     }, [user])
+}
+
+/**
+ * 
+ * @param {Array<number>}} lookup 
+ * @param {{ id: number, name: string, joinDate: "string", transactions: Array<{id: number, amount: number, date: string}>}} transaction 
+ * @returns {any}
+ */
+function groupTransactionsByMonth(lookup, transaction) {
+    const monthValue = moment(transaction.date).month();
+
+    if (!lookup[monthValue]) {
+        lookup[monthValue] = 0;
+    }
+
+    const rebateAmount = calculateRebate(transaction);
+    lookup[monthValue] += rebateAmount;
+
+    return lookup;
+}
+
+/**
+ * 
+ * @param { number } rebateAmount 
+ * @param { number } monthValue 
+ * @returns {{ month: string; rebateAmount: number }}
+ */
+function mapTotalRebatesPerMonth(rebateAmount, monthValue) {
+    return {
+        month: getMonthName(monthValue),
+        rebateAmount
+    }
 }
